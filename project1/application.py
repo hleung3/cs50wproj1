db3adf0c18c38da6fa89804174ffd2148a71135e
Harry Leung <42971819+hleung3@users.noreply.github.com>
2020-06-08 15:59:20 -0400

    created review and book pages and functions

--- project1/application.py
+++ project1/application.py
@@ -9,8 +9,10 @@ import requests
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 ## TODO: implement security features (i.e. HASHing passwords)
-## TODO: AUTOMATICALLY GETBOOKS AND UPDATE DB?
 ## TODO: should I move some functions to a helper function
+## TODO: ADD HOME BUTTON TO PAGES
+## TODO: ADD BOOTSTRAP ELEMentss
+## what happens to reviews when you delete a user?
 app = Flask(__name__, instance_relative_config=False)
 
 # Check for environment variable
@@ -36,7 +38,7 @@ db = scoped_session(sessionmaker(bind=engine))
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        print(session)
+        # print(session)
         if not session.get("logged_in"):
             session.pop("username",None)
             session.pop("logged_in",None)
@@ -55,7 +57,6 @@ def index():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     session.clear()
-    print(session)
     username = request.form.get("username")
     password = request.form.get("password")
     if request.method == 'POST':
@@ -63,19 +64,28 @@ def login():
         user_query = db.execute("SELECT * FROM users" +
                             " where username = :username",
                             {"username":username}).fetchone()
-        flash(str(user_query))
+        # flash(str(user_query))
         if user_query != None and password == user_query[2]:
             print(password,user_query[2])
             session['username'] = username
+            session['user_id'] = user_query[0]
             session['logged_in'] = True
-            session.modified = True
+            # session.modified = True
             return redirect("/")
         else:
             # TODO:
+            message = ""
+            if "" in (username,password): message += "Username cannot be empty. \n"
+            if user_query is None: message += "User does not exist \n"
+            elif password != user_query[2]: message += "Incorrect Password \n"
             # I think it will be better to
             #  display an error message on login page
             # error in login go to error page
-            return abort(401)
+            # return abort(401)
+            print(message)
+            flash(message)
+            # return redirect('/login')
+            return render_template("login.html")
     else:
         return render_template("login.html")
 
@@ -110,7 +120,7 @@ def register():
         " where username = :username",
         {"username":username}).fetchone()
 
-        flash(str(user_query))
+        # flash(str(user_query))
         if not username or username == "":
             return render_template("register.html",user_msg="please provide a username")
         elif user_query is not None:
@@ -129,24 +139,19 @@ def register():
         db.execute("INSERT INTO users (username,password) values (:username,:password)",
                      {"username":username, "password":password})
         db.commit()
-        ## TODO: update page to incidate success
-            ## flash bootstrap alert message ???
 
-        ## THEN: REDIRECT TO SUCCESS PAGE WITH HYPERLINK TO LOGIN
-        ##
-        return render_template("register.html",message="register success!",success=True)
+        ## MAYBE: flash bootstrap alert message ???
+        return render_template("register.html",message="register  success!",success=True)
 
-    # else:
-    #
-    #     # I think it will be better to
-    #     #  display an error message on login page
-    #     # error in login go to error page
-    #     return render_template("register.html")
+    else:
+
+        return render_template("register.html")
 
 @app.route("/search", methods=["GET"])
 @login_required
 def search():
     book = request.args.get("book")
+    ## # TODO: separate search to four for isbn title author year
     if not book:
         pass
     query = "%" + book + "%"
@@ -154,27 +159,27 @@ def search():
     # Capitalize all words of input for search
     # https://docs.python.org/3.7/library/stdtypes.html?highlight=title#str.title
     query = query.title()
+    print(query)
+    # # TODO: ADD % to each word manually
 
     rows = db.execute("SELECT isbn, title, author, year FROM books WHERE \
                         isbn LIKE :query OR \
                         title LIKE :query OR \
-                        author LIKE :query LIMIT 15",
+                        author LIKE :query LIMIT 10",
                         {"query": query})
-
+    print(rows.rowcount)
     # Books not found
     if rows.rowcount == 0:
-        return render_template("error.html", message="we can't find books with that description.")
+        return render_template("index.html", message="we can't find books with that description.")
 
     # Fetch all the results
     results = rows.fetchall()
 
-    # return render_template("results.html", books=results)
-    pass
+    return render_template("searchresults.html", books=results)
 
 @app.route("/book/<isbn>",methods=["GET","POST"])
 @login_required
 def book(isbn):
-    print("AFDG")
     '''
         save user review and load same page with updated reviews
     '''
@@ -183,34 +188,64 @@ def book(isbn):
         rating = request.form.get("rating")
         comment = request.form.get("comment")
 
-        row = db.execute("SELECT id FROM books WHERE isbn = :isbn",
+        row = db.execute("SELECT index FROM books WHERE isbn = :isbn",
                          {"isbn": isbn})
         book = row.fetchone()
         bookID = book[0]
         # check for prior user submission
-        row2 = ""
-
-        if row2.rowcount == 1:
-            return redirect("/book/" + isbn)
+        row2 = db.execute("SELECT * FROM reviews WHERE user_id = :user_id" +
+                          " AND book_id = CAST(:book_id as varchar(10))",
+                          {"user_id": currentUser, "book_id":bookID})
+        # a review already counts
+        print(row2.rowcount)
+        if row2.rowcount > 0:
+            ## GO TO ERROR.HTML AND GIVE A REDIRECT BACK TO THIS BOOK
+            flash('you have already reviewed this book')
+            return render_template("error.html", message="please try again",link="/book/"+isbn)
+            # return redirect("/book/" + isbn)
 
         rating = int(rating)
-        # db.execute()
-        # db.commit()
-        # submitted message
+        db.execute("INSERT INTO reviews (user_id, book_id, text, rating) VALUES \
+                    (:user_id, :book_id, :text, :rating)",
+                    {"user_id": currentUser,
+                    "book_id": bookID,
+                    "text": comment,
+                    "rating": rating})
+        db.commit()
 
+        
         return redirect("/book/" + isbn)
     else:
-        # row = db.execute()
-        # bookinfo = row.fetchall()
+        ## METHOD == GET --> REVIEWS FOR BOOK(USING ISBN CODE AND API)
+
+        row = db.execute("SELECT isbn, title, author, year FROM books WHERE \
+                        isbn = :isbn",
+                        {"isbn": isbn})
+        bookinfo = row.fetchall()
         ''' good reads review '''
-        api_key = ""
-        query = ""
-        response = ""
-        bookinfo = ""
+        api_key = os.getenv("GOODREADS_KEY")
+        query = requests.get("https://www.goodreads.com/book/review_counts.json",
+                params={"key": api_key, "isbns": isbn})
+        response = query.json()
+        response = response['books'][0]
+        bookinfo.append(response)
+
         # get books
-        # row = db.execute()
-        # book = row.fetchone()
-        pass
+        row = db.execute("SELECT CAST(index as varchar(10)) FROM books WHERE isbn = :isbn",
+                        {"isbn": isbn})
+        book = row.fetchone()[0]
+        results = db.execute("SELECT users.username, text, rating, \
+                            to_char(date, 'DD Mon YY - HH24:MI:SS') as time \
+                            FROM users \
+                            INNER JOIN reviews \
+                            ON users.id = reviews.user_id \
+                            WHERE reviews.book_id = :book \
+                            ORDER BY time",
+                            {"book": book})
+
+        reviews = results.fetchall()
+
+        return render_template("bookpage.html", bookInfo=bookinfo, reviews=reviews)
 
 
 
@@ -227,11 +262,11 @@ def api_call(isbn):
                     AVG(reviews.rating) as average_score \
                     FROM books \
                     INNER JOIN reviews \
-                    ON books.id = reviews.book_id \
+                    ON CAST(books.index as varchar(10)) = reviews.book_id \
                     WHERE isbn = :isbn \
                     GROUP BY title, author, year, isbn",
-                    {"isbn": isbn})
-
+                    {"isbn": isbn}).fetchone()
+    print(row)
     # Error checking
     if row.rowcount != 1:
         return jsonify({"Error": "Invalid book ISBN"}), 422
